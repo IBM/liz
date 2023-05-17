8ec1cea047e484b158c7fd5e50cae50b2205728b
Daniel Haischt <modean@linux.ibm.com>
2023-05-17 16:51:35 +0200

    fix: add support for misc params panel

--- src/components/panels/MiscParameters/MiscParameters.jsx
+++ src/components/panels/MiscParameters/MiscParameters.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { TextArea, Grid, Column } from "@carbon/react";
 
 const MiscParameters = () => {
-  return <div>Misc Parameters Panel</div>;
+  return (
+    <Grid className="misc-parameters_grid" fullWidth>
+      <Column sm={6} md={8} lg={16}>
+        <TextArea
+          labelText=""
+          helperText=""
+          rows={8}
+          id="misc-parameters-input"
+        />
+      </Column>
+    </Grid>
+  );
 };
 
 export default MiscParameters;
