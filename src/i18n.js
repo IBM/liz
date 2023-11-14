c01fbcb761dcd69baf131e0ab715ba8afed9f1ec
Daniel Haischt <modean@linux.ibm.com>
2023-11-14 21:16:58 +0100

    fix: comment out i18n backend

--- src/i18n.js
+++ src/i18n.js
@@ -7,7 +7,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+// import Backend from "i18next-http-backend";
 
 import enTranslationJSON from "./locales/en/translation.json";
 import deTranslationJSON from "./locales/de/translation.json";
@@ -17,7 +17,7 @@ import enHelpJSON from "./locales/en/help.json";
 import deHelpJSON from "./locales/de/help.json";
 
 i18n
-  .use(Backend)
+  // .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   // for all options read: https://www.i18next.com/overview/configuration-options
@@ -32,18 +32,18 @@ i18n
     preload: ["en", "de"],
     ns: ["translation", "common", "help"],
     defaultNS: "translation",
-    backend: {
-      // load from GHE liz repo
-      loadPath:
-        "https://raw.github.ibm.com/linuxonz/liz/main/src/locales/{{lng}}/{{ns}}.json",
-      crossDomain: true,
-      requestOptions: {
-        // used for fetch, can also be a function (payload) => ({ method: 'GET' })
-        mode: "no-cors",
-        credentials: "same-origin",
-        cache: "default",
-      },
-    },
+    // backend: {
+    //   // load from GHE liz repo
+    //   loadPath:
+    //     "https://raw.github.ibm.com/linuxonz/liz/main/src/locales/{{lng}}/{{ns}}.json",
+    //   crossDomain: true,
+    //   requestOptions: {
+    //     // used for fetch, can also be a function (payload) => ({ method: 'GET' })
+    //     mode: "no-cors",
+    //     credentials: "same-origin",
+    //     cache: "default",
+    //   },
+    // },
   });
 
 export default i18n;
