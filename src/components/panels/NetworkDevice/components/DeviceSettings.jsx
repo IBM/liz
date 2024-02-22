eb9a7f7807ab724ee72c22da836238f1aedb3a29
Daniel Haischt <daniel.haischt@de.ibm.com>
2024-02-22 16:00:55 +0100

    fix: use flex grid (#701)
    Co-authored-by: Daniel Haischt <modean@linux.ibm.com>

--- src/components/panels/NetworkDevice/components/DeviceSettings.jsx
+++ src/components/panels/NetworkDevice/components/DeviceSettings.jsx
@@ -10,8 +10,9 @@ import PropTypes from "prop-types";
 import {
   TextInput,
   Toggle,
-  Grid,
+  FlexGrid,
   Column,
+  Row,
   RadioButtonGroup,
   RadioButton,
 } from "@carbon/react";
@@ -74,76 +75,80 @@ const DeviceSettings = ({ deviceSettingsId, updateFunction, state }) => {
 
   const osaMarkup = (
     <>
-      <Grid className="device-settings_grid" condensed>
-        <Column sm={4} md={5} max={6}>
-          <div className="device-settings_grid-column-left">
-            <Toggle
-              defaultToggled
-              toggled={state.layer}
-              labelText={t("panel.networkDevice.layerTwoToggleTextLabel", {
-                ns: "panels",
-              })}
-              labelA={t("panel.networkDevice.layerTwoToggleTextLabelA", {
-                ns: "panels",
-              })}
-              labelB={t("panel.networkDevice.layerTwoToggleTextLabelB", {
-                ns: "panels",
-              })}
-              id="layer2-toggle"
-              onToggle={(toggleState) => {
-                updateFunction(UPDATE_FUNCTION__LAYER, toggleState);
-              }}
-            />
-          </div>
-        </Column>
-        <Column sm={4} md={5} max={6}>
-          <div className="device-settings_grid-column-right">
-            <Toggle
-              toggled={state.useMultiPort}
-              labelText={t("panel.networkDevice.multiPortToggleTextLabel", {
-                ns: "panels",
-              })}
-              labelA={t("btnLabel.No", { ns: "common" })}
-              labelB={t("btnLabel.Yes", { ns: "common" })}
-              id="portno-toggle"
-              onToggle={(toggleState) => {
-                updateFunction(UPDATE_FUNCTION__USE_MULTIPORT, toggleState);
-              }}
-            />
-          </div>
-        </Column>
-      </Grid>
-      {state.useMultiPort && (
-        <Grid className="device-settings_grid" condensed>
-          <Column sm={8} md={10} max={12}>
-            <RadioButtonGroup
-              className="network-device_port-number-radiobutton-group"
-              legendText={t("panel.networkDevice.portNumberToggleTextLabel", {
-                ns: "panels",
-              })}
-              helperText={t("panel.networkDevice.portNumberToggleHelp", {
-                ns: "panels",
-              })}
-              name="network-device_port-number-group"
-              defaultSelected={getDefaultSelected()}
-              onChange={(selectedItem) => {
-                const portOneToggled = selectedItem === PORT_NUMBER_ONE;
-                updateFunction(UPDATE_FUNCTION__PORT_NO, portOneToggled);
-              }}
-            >
-              <RadioButton
-                labelText="Port 0"
-                value={PORT_NUMBER_ZERO}
-                id="network-device_portno-zero-radio"
+      <FlexGrid className="device-settings_grid" condensed>
+        <Row>
+          <Column>
+            <div className="device-settings_grid-column-left">
+              <Toggle
+                defaultToggled
+                toggled={state.layer}
+                labelText={t("panel.networkDevice.layerTwoToggleTextLabel", {
+                  ns: "panels",
+                })}
+                labelA={t("panel.networkDevice.layerTwoToggleTextLabelA", {
+                  ns: "panels",
+                })}
+                labelB={t("panel.networkDevice.layerTwoToggleTextLabelB", {
+                  ns: "panels",
+                })}
+                id="layer2-toggle"
+                onToggle={(toggleState) => {
+                  updateFunction(UPDATE_FUNCTION__LAYER, toggleState);
+                }}
               />
-              <RadioButton
-                labelText="Port 1"
-                value={PORT_NUMBER_ONE}
-                id="network-device_portno-one-radio"
-              />
-            </RadioButtonGroup>
+            </div>
           </Column>
-        </Grid>
+          <Column>
+            <div className="device-settings_grid-column-right">
+              <Toggle
+                toggled={state.useMultiPort}
+                labelText={t("panel.networkDevice.multiPortToggleTextLabel", {
+                  ns: "panels",
+                })}
+                labelA={t("btnLabel.No", { ns: "common" })}
+                labelB={t("btnLabel.Yes", { ns: "common" })}
+                id="portno-toggle"
+                onToggle={(toggleState) => {
+                  updateFunction(UPDATE_FUNCTION__USE_MULTIPORT, toggleState);
+                }}
+              />
+            </div>
+          </Column>
+        </Row>
+      </FlexGrid>
+      {state.useMultiPort && (
+        <FlexGrid className="device-settings_grid" condensed>
+          <Row>
+            <Column>
+              <RadioButtonGroup
+                className="network-device_port-number-radiobutton-group"
+                legendText={t("panel.networkDevice.portNumberToggleTextLabel", {
+                  ns: "panels",
+                })}
+                helperText={t("panel.networkDevice.portNumberToggleHelp", {
+                  ns: "panels",
+                })}
+                name="network-device_port-number-group"
+                defaultSelected={getDefaultSelected()}
+                onChange={(selectedItem) => {
+                  const portOneToggled = selectedItem === PORT_NUMBER_ONE;
+                  updateFunction(UPDATE_FUNCTION__PORT_NO, portOneToggled);
+                }}
+              >
+                <RadioButton
+                  labelText="Port 0"
+                  value={PORT_NUMBER_ZERO}
+                  id="network-device_portno-zero-radio"
+                />
+                <RadioButton
+                  labelText="Port 1"
+                  value={PORT_NUMBER_ONE}
+                  id="network-device_portno-one-radio"
+                />
+              </RadioButtonGroup>
+            </Column>
+          </Row>
+        </FlexGrid>
       )}
     </>
   );
@@ -162,104 +167,108 @@ const DeviceSettings = ({ deviceSettingsId, updateFunction, state }) => {
   };
 
   const roceMarkup = (
-    <Grid className="device-settings_grid" fullWidth>
-      <Column sm={2} md={8} lg={16}>
-        <div className="device-settings_grid-column-single">
-          <TextInput
-            id="pci-function-input"
-            invalidText={t("invalidTextLabel", { ns: "common" })}
-            invalid={
-              state && state.pciFunctionId ? !state.pciFunctionId.valid : false
-            }
-            labelText={roceLabelHasOptionalTag(
-              "userIdentifier",
-              t("panel.networkDevice.fidTextLabel", { ns: "panels" }),
-              t("panel.networkDevice.fidTextLabelOptional", { ns: "panels" }),
-            )}
-            helperText={t("panel.networkDevice.fidHelp", { ns: "panels" })}
-            placeholder={t("panel.networkDevice.fidPlaceholder", {
-              ns: "panels",
-            })}
-            value={state?.pciFunctionId?.value ?? ""}
-            onChange={(pciFunctionId) => {
-              const pciFunctionIdValue =
-                pciFunctionId &&
-                pciFunctionId.target &&
-                pciFunctionId.target.value
-                  ? pciFunctionId.target.value
-                  : "";
-              updateFunction(
-                UPDATE_FUNCTION__PCI_FUNCTION_ID,
-                pciFunctionIdValue,
-                true,
-              );
-            }}
-            onBlur={(pciFunctionId) => {
-              const pciFunctionIdValue =
-                pciFunctionId &&
-                pciFunctionId.target &&
-                pciFunctionId.target.value
-                  ? pciFunctionId.target.value
-                  : "";
-              const pciFunctionIdIsValid =
-                isPciFunctionIdValid(pciFunctionIdValue);
-              updateFunction(
-                UPDATE_FUNCTION__PCI_FUNCTION_ID,
-                pciFunctionIdValue,
-                pciFunctionIdIsValid,
-              );
-            }}
-          />
-          <TextInput
-            id="user-identifier-input"
-            invalidText={t("invalidTextLabel", { ns: "common" })}
-            invalid={
-              state && state.userIdentifier
-                ? !state.userIdentifier.valid
-                : false
-            }
-            labelText={roceLabelHasOptionalTag(
-              "pciFunctionId",
-              t("panel.networkDevice.uidTextLabel", { ns: "panels" }),
-              t("panel.networkDevice.uidTextLabelOptional", { ns: "panels" }),
-            )}
-            helperText={t("panel.networkDevice.uidHelp", { ns: "panels" })}
-            placeholder={t("panel.networkDevice.uidPlaceholder", {
-              ns: "panels",
-            })}
-            value={state?.userIdentifier?.value ?? ""}
-            onChange={(userIdentifier) => {
-              const userIdentifierValue =
-                userIdentifier &&
-                userIdentifier.target &&
-                userIdentifier.target.value
-                  ? userIdentifier.target.value
-                  : "";
-              updateFunction(
-                UPDATE_FUNCTION__USER_IDENTIFIER,
-                userIdentifierValue,
-                true,
-              );
-            }}
-            onBlur={(userIdentifier) => {
-              const userIdentifierValue =
-                userIdentifier &&
-                userIdentifier.target &&
-                userIdentifier.target.value
-                  ? userIdentifier.target.value
-                  : "";
-              const userIdentifierIsValid =
-                isUserIdentifierValid(userIdentifierValue);
-              updateFunction(
-                UPDATE_FUNCTION__USER_IDENTIFIER,
-                userIdentifierValue,
-                userIdentifierIsValid,
-              );
-            }}
-          />
-        </div>
-      </Column>
-    </Grid>
+    <FlexGrid className="device-settings_grid" fullWidth>
+      <Row>
+        <Column>
+          <div className="device-settings_grid-column-single">
+            <TextInput
+              id="pci-function-input"
+              invalidText={t("invalidTextLabel", { ns: "common" })}
+              invalid={
+                state && state.pciFunctionId
+                  ? !state.pciFunctionId.valid
+                  : false
+              }
+              labelText={roceLabelHasOptionalTag(
+                "userIdentifier",
+                t("panel.networkDevice.fidTextLabel", { ns: "panels" }),
+                t("panel.networkDevice.fidTextLabelOptional", { ns: "panels" }),
+              )}
+              helperText={t("panel.networkDevice.fidHelp", { ns: "panels" })}
+              placeholder={t("panel.networkDevice.fidPlaceholder", {
+                ns: "panels",
+              })}
+              value={state?.pciFunctionId?.value ?? ""}
+              onChange={(pciFunctionId) => {
+                const pciFunctionIdValue =
+                  pciFunctionId &&
+                  pciFunctionId.target &&
+                  pciFunctionId.target.value
+                    ? pciFunctionId.target.value
+                    : "";
+                updateFunction(
+                  UPDATE_FUNCTION__PCI_FUNCTION_ID,
+                  pciFunctionIdValue,
+                  true,
+                );
+              }}
+              onBlur={(pciFunctionId) => {
+                const pciFunctionIdValue =
+                  pciFunctionId &&
+                  pciFunctionId.target &&
+                  pciFunctionId.target.value
+                    ? pciFunctionId.target.value
+                    : "";
+                const pciFunctionIdIsValid =
+                  isPciFunctionIdValid(pciFunctionIdValue);
+                updateFunction(
+                  UPDATE_FUNCTION__PCI_FUNCTION_ID,
+                  pciFunctionIdValue,
+                  pciFunctionIdIsValid,
+                );
+              }}
+            />
+            <TextInput
+              id="user-identifier-input"
+              invalidText={t("invalidTextLabel", { ns: "common" })}
+              invalid={
+                state && state.userIdentifier
+                  ? !state.userIdentifier.valid
+                  : false
+              }
+              labelText={roceLabelHasOptionalTag(
+                "pciFunctionId",
+                t("panel.networkDevice.uidTextLabel", { ns: "panels" }),
+                t("panel.networkDevice.uidTextLabelOptional", { ns: "panels" }),
+              )}
+              helperText={t("panel.networkDevice.uidHelp", { ns: "panels" })}
+              placeholder={t("panel.networkDevice.uidPlaceholder", {
+                ns: "panels",
+              })}
+              value={state?.userIdentifier?.value ?? ""}
+              onChange={(userIdentifier) => {
+                const userIdentifierValue =
+                  userIdentifier &&
+                  userIdentifier.target &&
+                  userIdentifier.target.value
+                    ? userIdentifier.target.value
+                    : "";
+                updateFunction(
+                  UPDATE_FUNCTION__USER_IDENTIFIER,
+                  userIdentifierValue,
+                  true,
+                );
+              }}
+              onBlur={(userIdentifier) => {
+                const userIdentifierValue =
+                  userIdentifier &&
+                  userIdentifier.target &&
+                  userIdentifier.target.value
+                    ? userIdentifier.target.value
+                    : "";
+                const userIdentifierIsValid =
+                  isUserIdentifierValid(userIdentifierValue);
+                updateFunction(
+                  UPDATE_FUNCTION__USER_IDENTIFIER,
+                  userIdentifierValue,
+                  userIdentifierIsValid,
+                );
+              }}
+            />
+          </div>
+        </Column>
+      </Row>
+    </FlexGrid>
   );
 
   if (deviceSettingsId === "network-device_osa-option") {
