dfa3835fa76a6b797a8eaec6420172097a9de801
Daniel Haischt <daniel.haischt@de.ibm.com>
2024-05-13 17:58:03 +0200

    fix: remove unsupported locales (#967)
    Signed-off-by: Daniel Haischt <modean@linux.ibm.com>
    Co-authored-by: Daniel Haischt <modean@linux.ibm.com>

--- src/i18n.js
+++ src/i18n.js
@@ -4,40 +4,39 @@
  * (C) Copyright IBM Corp. 2023
  */
 
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
-import Pseudo from "i18next-pseudo";
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
+import Pseudo from 'i18next-pseudo'
 
 const i18Init = () => {
-  i18n
-    .use(
-      new Pseudo({
-        enabled: true,
-        languageToPseudo: "es-US",
-        letterMultiplier: 4,
-        repeatedLetters: ["B", "o", "a", "t"],
-      }),
+    i18n.use(
+        new Pseudo({
+            enabled: true,
+            languageToPseudo: 'es-US',
+            letterMultiplier: 4,
+            repeatedLetters: ['B', 'o', 'a', 't'],
+        })
     )
-    .use(Backend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    // for all options read: https://www.i18next.com/overview/configuration-options
-    .init({
-      debug: true,
-      fallbackLng: "en",
-      preload: ["en", "de"],
-      ns: ["translation", "panels", "common", "help"],
-      defaultNS: "translation",
-      backend: {
-        loadPath: `${import.meta.env.VITE_URL_PATH_PREFIX}locales/{{lng}}/{{ns}}.json`,
-      },
-      detection: {
-        order: ["querystring", "cookie", "navigator", "htmlTag"],
-      },
-      postProcess: ["pseudo"],
-    });
-};
+        .use(Backend)
+        .use(LanguageDetector)
+        .use(initReactI18next)
+        // for all options read: https://www.i18next.com/overview/configuration-options
+        .init({
+            debug: true,
+            fallbackLng: 'en',
+            preload: ['en'],
+            ns: ['translation', 'panels', 'common', 'help'],
+            defaultNS: 'translation',
+            backend: {
+                loadPath: `${import.meta.env.VITE_URL_PATH_PREFIX}locales/{{lng}}/{{ns}}.json`,
+            },
+            detection: {
+                order: ['querystring', 'cookie', 'navigator', 'htmlTag'],
+            },
+            postProcess: ['pseudo'],
+        })
+}
 
-export default i18Init;
+export default i18Init
