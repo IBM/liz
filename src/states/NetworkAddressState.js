9f509ef08e011eace0fc93eeeb3dd0df34cabdf5
Daniel Haischt <daniel.haischt@de.ibm.com>
2024-05-23 17:29:45 +0200

    fix: default cidr for ipv6 (#1012)
    Signed-off-by: Daniel Haischt <modean@linux.ibm.com>
    Co-authored-by: Daniel Haischt <modean@linux.ibm.com>

--- src/states/NetworkAddressState.js
+++ src/states/NetworkAddressState.js
@@ -5,53 +5,53 @@
  */
 
 import {
-  LOCAL_STORAGE_KEY_APP_NETWORK_ADDRESS,
-  STATE_ORIGIN_DEFAULT,
-} from "../util/local-storage-constants";
-import { DEFAULT_STRING_OBJECT, ADDRESS_TYPE_IPV4 } from "../util/constants";
-import { getItem } from "../util/local-storage-util";
+    LOCAL_STORAGE_KEY_APP_NETWORK_ADDRESS,
+    STATE_ORIGIN_DEFAULT,
+} from '../util/local-storage-constants'
+import { DEFAULT_STRING_OBJECT, ADDRESS_TYPE_IPV4 } from '../util/constants'
+import { getItem } from '../util/local-storage-util'
 
 const createInitialState = (skipLocalStorageUsage = false) => {
-  const initialState = getItem(LOCAL_STORAGE_KEY_APP_NETWORK_ADDRESS);
-  const defaultState = {
-    ipv4: {
-      netmask: {
-        value: "255.255.255.0",
-        valid: true,
-        computed: false,
-      },
-      ipv4Cidr: {
-        value: 24,
-        valid: true,
-        computed: false,
-      },
-      binary: "11111111.11111111.11111111.00000000",
-      ipv4Address: DEFAULT_STRING_OBJECT,
-      gatewayIpAddress: DEFAULT_STRING_OBJECT,
-      nameserverIpAddress: DEFAULT_STRING_OBJECT,
-      hostName: DEFAULT_STRING_OBJECT,
-      domainSearchPath: DEFAULT_STRING_OBJECT,
-    },
-    ipv6: {
-      ipv6Cidr: {
-        value: 32,
-        valid: true,
-      },
-      ipv6Address: DEFAULT_STRING_OBJECT,
-      gatewayIpAddress: DEFAULT_STRING_OBJECT,
-      nameserverIpAddress: DEFAULT_STRING_OBJECT,
-      hostName: DEFAULT_STRING_OBJECT,
-      domainSearchPath: DEFAULT_STRING_OBJECT,
-    },
-    addressType: ADDRESS_TYPE_IPV4,
-    origin: STATE_ORIGIN_DEFAULT,
-  };
+    const initialState = getItem(LOCAL_STORAGE_KEY_APP_NETWORK_ADDRESS)
+    const defaultState = {
+        ipv4: {
+            netmask: {
+                value: '255.255.255.0',
+                valid: true,
+                computed: false,
+            },
+            ipv4Cidr: {
+                value: 24,
+                valid: true,
+                computed: false,
+            },
+            binary: '11111111.11111111.11111111.00000000',
+            ipv4Address: DEFAULT_STRING_OBJECT,
+            gatewayIpAddress: DEFAULT_STRING_OBJECT,
+            nameserverIpAddress: DEFAULT_STRING_OBJECT,
+            hostName: DEFAULT_STRING_OBJECT,
+            domainSearchPath: DEFAULT_STRING_OBJECT,
+        },
+        ipv6: {
+            ipv6Cidr: {
+                value: 64,
+                valid: true,
+            },
+            ipv6Address: DEFAULT_STRING_OBJECT,
+            gatewayIpAddress: DEFAULT_STRING_OBJECT,
+            nameserverIpAddress: DEFAULT_STRING_OBJECT,
+            hostName: DEFAULT_STRING_OBJECT,
+            domainSearchPath: DEFAULT_STRING_OBJECT,
+        },
+        addressType: ADDRESS_TYPE_IPV4,
+        origin: STATE_ORIGIN_DEFAULT,
+    }
 
-  if (initialState && !skipLocalStorageUsage) {
-    return initialState;
-  }
+    if (initialState && !skipLocalStorageUsage) {
+        return initialState
+    }
 
-  return defaultState;
-};
+    return defaultState
+}
 
-export { createInitialState };
+export { createInitialState }
