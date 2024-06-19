ee1ce70ed62e75fd9469e764167c4025de062736
Daniel Haischt <daniel.haischt@de.ibm.com>
2024-06-19 19:23:48 +0200

    fix: parmfile validation logic for sles (#1070)
    Signed-off-by: Daniel Haischt <modean@linux.ibm.com>
    Co-authored-by: Daniel Haischt <modean@linux.ibm.com>

--- src/util/param-file-util_sles.js
+++ src/util/param-file-util_sles.js
@@ -8,18 +8,9 @@ import { DEVICE_TYPE_OSA } from './constants'
 
 import {
     stateToNetworkAddressParams as stateToNetworkAddressParamsSlesOSA,
-    stateToNetworkDeviceParams as stateToNetworkDeviceParamsSlesOSA,
-    stateToSshParams as stateToSshParamsSlesOSA,
-    stateToVncParams as stateToVncParamsSlesOSA,
-    stateToInstallationRepoParams as stateToInstallationRepoParamsSlesOSA,
+    stateToOsaNetworkDeviceParams,
 } from './param-file-util_sles_osa'
-import {
-    stateToNetworkAddressParams as stateToNetworkAddressParamsSlesRoCE,
-    stateToNetworkDeviceParams as stateToNetworkDeviceParamsSlesRoCE,
-    stateToSshParams as stateToSshParamsSlesRoCE,
-    stateToVncParams as stateToVncParamsSlesRoCE,
-    stateToInstallationRepoParams as stateToInstallationRepoParamsSlesRoCE,
-} from './param-file-util_sles_roce'
+import { stateToNetworkAddressParams as stateToNetworkAddressParamsSlesRoCE } from './param-file-util_sles_roce'
 
 const stateToNetworkAddressParams = (state) => {
     const deviceType =
@@ -30,35 +21,123 @@ const stateToNetworkAddressParams = (state) => {
 }
 
 const stateToNetworkDeviceParams = (state) => {
-    const deviceType =
-        state?.steps?.networkDevice?.deviceType ?? DEVICE_TYPE_OSA
-    return deviceType === DEVICE_TYPE_OSA
-        ? stateToNetworkDeviceParamsSlesOSA(state)
-        : stateToNetworkDeviceParamsSlesRoCE(state)
+    const installationParameters = state?.steps?.networkDevice ?? {}
+    const networkDeviceSettings =
+        installationParameters.deviceType === DEVICE_TYPE_OSA
+            ? stateToOsaNetworkDeviceParams(installationParameters)
+            : ``
+    let paramFileContents = {
+        contents: '',
+        complete: false,
+        label: '',
+        index: 0,
+    }
+
+    // => rd.znet...
+    if (installationParameters) {
+        const installationRepoLine = `${networkDeviceSettings}`
+        paramFileContents = {
+            contents: `${installationRepoLine}`,
+            complete: installationParameters.complete,
+            invalid: installationParameters.invalid,
+            index: installationParameters.index,
+        }
+    }
+
+    return paramFileContents
 }
 
 const stateToInstallationRepoParams = (state) => {
-    const deviceType =
-        state?.steps?.networkDevice?.deviceType ?? DEVICE_TYPE_OSA
-    return deviceType === DEVICE_TYPE_OSA
-        ? stateToSshParamsSlesOSA(state)
-        : stateToSshParamsSlesRoCE(state)
+    const installationParameters = state?.steps?.installationParameters ?? {}
+    const networkInstallationUrl =
+        installationParameters?.networkInstallationUrl ?? ''
+    let paramFileContents = {
+        contents: '',
+        complete: false,
+        index: 0,
+    }
+
+    // => inst.repo=...
+    const installationRepoLine = `install=${networkInstallationUrl}`
+    paramFileContents = {
+        contents: `${installationRepoLine}`,
+        complete: installationParameters.complete,
+        invalid: installationParameters.invalid,
+        index: installationParameters.index,
+    }
+
+    return paramFileContents
 }
 
 const stateToVncParams = (state) => {
-    const deviceType =
-        state?.steps?.networkDevice?.deviceType ?? DEVICE_TYPE_OSA
-    return deviceType === DEVICE_TYPE_OSA
-        ? stateToVncParamsSlesOSA(state)
-        : stateToVncParamsSlesRoCE(state)
+    const installationParameters = state?.steps?.installationParameters ?? {}
+    let paramFileContents = {
+        contents: '',
+        complete: false,
+        index: 0,
+    }
+
+    // => inst.vnc inst.vncpassword=...
+    if (
+        installationParameters &&
+        installationParameters.vnc &&
+        installationParameters.vnc.enabled === true
+    ) {
+        if (
+            installationParameters.vnc.password &&
+            installationParameters.vnc.password.value &&
+            installationParameters.vnc.password.value.length > 0
+        ) {
+            const vncServerLine = `vnc=1 vncpassword=${installationParameters.vnc.password.value}`
+            paramFileContents = {
+                contents: `${vncServerLine}`,
+                complete: installationParameters.complete,
+                invalid: installationParameters.invalid,
+                index: installationParameters.index,
+            }
+        } else {
+            const vncServerLine = `vnc=1`
+            paramFileContents = {
+                contents: `${vncServerLine}`,
+                complete: installationParameters.complete,
+                invalid: installationParameters.invalid,
+                index: installationParameters.index,
+            }
+        }
+    }
+
+    return paramFileContents
 }
 
 const stateToSshParams = (state) => {
-    const deviceType =
-        state?.steps?.networkDevice?.deviceType ?? DEVICE_TYPE_OSA
-    return deviceType === DEVICE_TYPE_OSA
-        ? stateToInstallationRepoParamsSlesOSA(state)
-        : stateToInstallationRepoParamsSlesRoCE(state)
+    const installationParameters = state?.steps?.installationParameters ?? {}
+    let paramFileContents = {
+        contents: '',
+        complete: false,
+        index: 0,
+    }
+
+    // => inst.sshd
+    if (
+        installationParameters &&
+        installationParameters.ssh &&
+        installationParameters.ssh.enabled === true
+    ) {
+        const hasSshPassword = !!(
+            installationParameters.ssh.password &&
+            installationParameters.ssh.password.value &&
+            installationParameters.ssh.password.value.length > 0
+        )
+        const sshServerLine = `ssh=1${hasSshPassword ? ` ssh.password=${installationParameters.ssh.password.value}` : ''}`
+        paramFileContents = {
+            contents: `${sshServerLine}`,
+            complete: installationParameters.complete,
+            invalid: installationParameters.invalid,
+            index: installationParameters.index,
+        }
+    }
+
+    return paramFileContents
 }
 
 export {
