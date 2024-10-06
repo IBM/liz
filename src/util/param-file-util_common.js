f1d1d5eee3fad34ddd471b2d3ce1a20909b915be
Daniel Haischt <daniel.haischt@de.ibm.com>
2024-10-06 20:58:03 +0200

    Revert "Upstream (#1315)" (#1316)
    This reverts commit dccbb990934be2fd826faeca592735ac306c4ca9.

--- src/util/param-file-util_common.js
+++ src/util/param-file-util_common.js
@@ -4,57 +4,57 @@
  * (C) Copyright IBM Corp. 2024
  */
 
-import { getInterfaceName } from "./network-device-util";
-import { DEVICE_TYPE_OSA } from "./constants";
+import { getInterfaceName } from './network-device-util'
+import { DEVICE_TYPE_OSA } from './constants'
 
 const getInterfaceNameFromFidUid = (installationParameters) => {
-    const fid = installationParameters?.roce?.fid ?? "";
-    const uid = installationParameters?.roce?.uid ?? "";
+    const fid = installationParameters?.roce?.fid ?? ''
+    const uid = installationParameters?.roce?.uid ?? ''
 
-    return getInterfaceName("", fid, uid);
-};
+    return getInterfaceName('', fid, uid)
+}
 
 const getInterfaceNameFromReadChannel = (installationParameters) => {
-    const readChannel = installationParameters?.osa?.readChannel;
+    const readChannel = installationParameters?.osa?.readChannel
 
-    return getInterfaceName(readChannel);
-};
+    return getInterfaceName(readChannel)
+}
 
 const getInterfaceNameParamContents = (installationParameters) => {
     const interfaceName =
         installationParameters.deviceType === DEVICE_TYPE_OSA
             ? getInterfaceNameFromReadChannel(installationParameters)
-            : getInterfaceNameFromFidUid(installationParameters);
+            : getInterfaceNameFromFidUid(installationParameters)
 
-    return interfaceName;
-};
+    return interfaceName
+}
 
-const getNetdevName = (vlanId = "", interfaceName = "") => {
+const getNetdevName = (vlanId = '', interfaceName = '') => {
     if (
         vlanId &&
-        typeof vlanId === "number" &&
+        typeof vlanId === 'number' &&
         interfaceName &&
-        typeof interfaceName === "string" &&
+        typeof interfaceName === 'string' &&
         interfaceName.length > 0
     ) {
-        return `${getVlanName(interfaceName, vlanId)}`;
+        return `${getVlanName(interfaceName, vlanId)}`
     } else if (
         interfaceName &&
-        typeof interfaceName === "string" &&
+        typeof interfaceName === 'string' &&
         interfaceName.length > 0
     ) {
-        return interfaceName;
+        return interfaceName
     }
 
-    return ``;
-};
+    return ``
+}
 
 const getVlanName = (interfaceName, vlanId) => {
     if (interfaceName && vlanId) {
-        return `${interfaceName}.${vlanId}`;
+        return `${interfaceName}.${vlanId}`
     }
-    return ``;
-};
+    return ``
+}
 
 export {
     getVlanName,
@@ -62,4 +62,4 @@ export {
     getInterfaceNameFromFidUid,
     getInterfaceNameFromReadChannel,
     getInterfaceNameParamContents,
-};
+}
