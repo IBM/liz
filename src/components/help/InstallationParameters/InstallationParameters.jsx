5c317a429503f2c88f228be7faa3024badb5ae31
Daniel Haischt <daniel.haischt@de.ibm.com>
2024-10-07 09:04:16 +0200

    fix: add distribution specific views (#1317)
    Signed-off-by: Daniel Haischt <modean@linux.ibm.com>
    Co-authored-by: Daniel Haischt <modean@linux.ibm.com>

--- src/components/help/InstallationParameters/InstallationParameters.jsx
+++ src/components/help/InstallationParameters/InstallationParameters.jsx
@@ -5,10 +5,10 @@
  */
 
 import React, { useContext } from "react";
+import loadable from "@loadable/component";
 import PropTypes from "prop-types";
-import { Trans } from "react-i18next";
 import { ApplicationContext } from "../../../contexts";
-import { UBUNTU_DISTRIBUTION_ID } from "../../../util/constants";
+import { InstallationParameters as CommonView } from "./common/InstallationParameters";
 import "./_installation-parameters.scss";
 
 const InstallationParameters = ({
@@ -17,74 +17,20 @@ const InstallationParameters = ({
     updateCurrentHelpStep,
 }) => {
     const { state: globalState } = useContext(ApplicationContext);
-
     const distributionName =
         globalState.steps.inputFileSelection.distributionName;
+    const DistributionView = loadable(
+        () =>
+            import(`./distribution/${distributionName}/InstallationParameters`),
+        {
+            resolveComponent: (components) => components.default,
+        }
+    );
 
     return (
         <>
-            <div
-                className="help-panel__installation-parameters__content"
-                id="helpPanelContents_installationParameters_para1"
-            >
-                <Trans
-                    i18nKey="helpPanelContents.installationParameters.para1"
-                    ns="help_installationParameters"
-                />
-            </div>
-            <div
-                className="help-panel__installation-parameters__content"
-                id="helpPanelContents_installationParameters_para2"
-            >
-                <Trans
-                    i18nKey="helpPanelContents.installationParameters.para2"
-                    ns="help_installationParameters"
-                />
-            </div>
-            <div
-                className="help-panel__installation-parameters__content"
-                id="helpPanelContents_installationParameters_para3"
-            >
-                <Trans
-                    i18nKey="helpPanelContents.installationParameters.para3"
-                    ns="help_installationParameters"
-                />
-            </div>
-            {distributionName !== UBUNTU_DISTRIBUTION_ID && (
-                <>
-                    <div
-                        className="help-panel__installation-parameters__content"
-                        id="helpPanelContents_installationParameters_para4"
-                    >
-                        <Trans
-                            i18nKey="helpPanelContents.installationParameters.para4"
-                            ns="help_installationParameters"
-                        />
-                    </div>
-                    <div
-                        className="help-panel__installation-parameters__content"
-                        id="helpPanelContents_installationParameters_para5"
-                    >
-                        <Trans
-                            i18nKey="helpPanelContents.installationParameters.para5"
-                            ns="help_installationParameters"
-                        />
-                    </div>
-                </>
-            )}
-            <div
-                className="help-panel__installation-parameters__content__bottom"
-                id="helpPanelContents_installationParameters_para6"
-            >
-                <Trans
-                    i18nKey="helpPanelContents.installationParameters.para6"
-                    ns={
-                        distributionName !== UBUNTU_DISTRIBUTION_ID
-                            ? "help_installationParameters"
-                            : "help_installationParameters_ubuntu"
-                    }
-                />
-            </div>
+            <CommonView />
+            <DistributionView />
         </>
     );
 };
