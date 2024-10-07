a19401c461118a9c75d0ea7ec9d141caf3aced47
Daniel Haischt <daniel.haischt@de.ibm.com>
2024-10-07 10:16:00 +0200

    Revert "fix: add distribution specific views (#1317)" (#1318)
    This reverts commit 5c317a429503f2c88f228be7faa3024badb5ae31.

--- src/components/help/NetworkDevice/NetworkDevice.jsx
+++ src/components/help/NetworkDevice/NetworkDevice.jsx
@@ -5,10 +5,13 @@
  */
 
 import React, { useContext } from "react";
-import loadable from "@loadable/component";
 import PropTypes from "prop-types";
+import { Trans } from "react-i18next";
 import { ApplicationContext } from "../../../contexts";
-import { NetworkDevice as CommonView } from "./common/NetworkDevice";
+import {
+    SLES_DISTRIBUTION_ID,
+    UBUNTU_DISTRIBUTION_ID,
+} from "../../../util/constants";
 import "./_network-device.scss";
 
 const NetworkDevice = ({
@@ -20,18 +23,76 @@ const NetworkDevice = ({
 
     const distributionName =
         globalState.steps.inputFileSelection.distributionName;
-    const DistributionView = loadable(
-        () => import(`./distribution/${distributionName}/NetworkDevice`),
-        {
-            resolveComponent: (components) => components.default,
-        }
-    );
 
     return (
         <>
-            <CommonView>
-                <DistributionView />
-            </CommonView>
+            <div
+                className="help-panel__network-device__content"
+                id="helpPanelContents_networkDevice_para1"
+            >
+                <Trans
+                    i18nKey="helpPanelContents.networkDevice.para1"
+                    ns="help_networkDevice"
+                />
+            </div>
+            <div
+                className="help-panel__network-device__content"
+                id="helpPanelContents_networkDevice_para2"
+            >
+                <Trans
+                    i18nKey="helpPanelContents.networkDevice.para2"
+                    ns="help_networkDevice"
+                />
+            </div>
+            <div
+                className="help-panel__network-device__content"
+                id="helpPanelContents_networkDevice_para3"
+            >
+                <Trans
+                    i18nKey="helpPanelContents.networkDevice.para3"
+                    ns="help_networkDevice"
+                />
+            </div>
+            <div
+                className="help-panel__network-device__content"
+                id="helpPanelContents_networkDevice_para4"
+            >
+                <Trans
+                    i18nKey="helpPanelContents.networkDevice.para4"
+                    ns="help_networkDevice"
+                />
+            </div>
+            {distributionName !== UBUNTU_DISTRIBUTION_ID && (
+                <div
+                    className="help-panel__network-device__content"
+                    id="helpPanelContents_networkDevice_para5"
+                >
+                    <Trans
+                        i18nKey="helpPanelContents.networkDevice.para5"
+                        ns="help_networkDevice"
+                    />
+                </div>
+            )}
+            {distributionName !== SLES_DISTRIBUTION_ID && (
+                <div
+                    className="help-panel__network-device__content"
+                    id="helpPanelContents_networkDevice_para6"
+                >
+                    <Trans
+                        i18nKey="helpPanelContents.networkDevice.para6"
+                        ns="help_networkDevice"
+                    />
+                </div>
+            )}
+            <div
+                className="help-panel__network-device__content__bottom"
+                id="helpPanelContents_networkDevice_para7"
+            >
+                <Trans
+                    i18nKey="helpPanelContents.networkDevice.para7"
+                    ns="help_networkDevice"
+                />
+            </div>
         </>
     );
 };
