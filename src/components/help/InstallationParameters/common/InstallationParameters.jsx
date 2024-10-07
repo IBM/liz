bee6a1b62212e3d15896507e2163beccab3788ea
Daniel Haischt <daniel.haischt@de.ibm.com>
2024-10-07 17:57:34 +0200

    fix: add distribution specific views (#1319)
    * fix: add distribution specific views
    
    Signed-off-by: Daniel Haischt <modean@linux.ibm.com>
    
    * fix: use react lazy instead of loadable
    
    Signed-off-by: Daniel Haischt <modean@linux.ibm.com>
    
    ---------
    
    Signed-off-by: Daniel Haischt <modean@linux.ibm.com>
    Co-authored-by: Daniel Haischt <modean@linux.ibm.com>

---
+++ src/components/help/InstallationParameters/common/InstallationParameters.jsx
@@ -0,0 +1,45 @@
+/*
+ * liz - Installation assistant for Linux on IBM Z
+ *
+ * (C) Copyright IBM Corp. 2024
+ */
+
+import React from "react";
+import { Trans } from "react-i18next";
+import "../_installation-parameters.scss";
+
+const InstallationParameters = () => {
+    return (
+        <>
+            <div
+                className="help-panel__installation-parameters__content"
+                id="helpPanelContents_installationParameters_para1"
+            >
+                <Trans
+                    i18nKey="helpPanelContents.installationParameters.para1"
+                    ns="help_installationParameters"
+                />
+            </div>
+            <div
+                className="help-panel__installation-parameters__content"
+                id="helpPanelContents_installationParameters_para2"
+            >
+                <Trans
+                    i18nKey="helpPanelContents.installationParameters.para2"
+                    ns="help_installationParameters"
+                />
+            </div>
+            <div
+                className="help-panel__installation-parameters__content"
+                id="helpPanelContents_installationParameters_para3"
+            >
+                <Trans
+                    i18nKey="helpPanelContents.installationParameters.para3"
+                    ns="help_installationParameters"
+                />
+            </div>
+        </>
+    );
+};
+
+export default InstallationParameters;
